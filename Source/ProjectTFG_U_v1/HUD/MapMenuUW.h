17801a2180e51d4f49c9cd86faabd4ab7e692b90
Jose A. Artero <34593747+JAArtero@users.noreply.github.com>
2023-04-18 21:09:53 +0200

    Añadir menu con mapa

---
+++ Source/ProjectTFG_U_v1/HUD/MapMenuUW.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuUW.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTTFG_U_V1_API UMenuUW : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
